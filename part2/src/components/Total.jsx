e72705c23af0d95720ff14d4505534d8c6846968
amgarvi <amparo@handpoint.com>
2026-02-19 14:11:15 +0100

    Exercise 2.4

--- part2/src/components/Total.jsx
+++ part2/src/components/Total.jsx
@@ -1,7 +1,7 @@
 const Total = ({parts}) => {
     const total = parts.reduce((sum, part) => sum + part.exercises, 0)
     return (
-        <p>Total of {total} exercises</p>
+        <p><b>Total of {total} exercises</b></p>
     )
 }
 
