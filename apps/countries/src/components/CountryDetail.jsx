f42978a6b8643ea293c7ca9cc7d881d7dd7593a0
AmparoGarcia84 <amgarvi84@gmail.com>
2026-03-03 00:05:18 +0100

    Exercise 2.20

--- apps/countries/src/components/CountryDetail.jsx
+++ apps/countries/src/components/CountryDetail.jsx
@@ -1,4 +1,15 @@
+import { useState, useEffect } from 'react'
+import weatherService from '../services/weather'
+
 const CountryDetail = ({ country }) => {
+    const [weather, setWeather] = useState(null)
+    useEffect(() => {
+        if (country && country.capital) {
+        weatherService.getWeather(country.capital[0]).then(weather => {
+                setWeather(weather)
+            })
+        }
+    }, [country])
     if (country === null) {
         return null
     }
@@ -12,6 +23,11 @@ const CountryDetail = ({ country }) => {
                 {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
             </ul>
             <img src={country.flags.png} alt={country.name.common} />
+            <h2>Weather in {country.capital[0]}</h2>
+        <p>Temperature: {weather?.main.temp} °C</p>
+            <p>Weather: {weather?.weather[0].description}</p>
+            <img src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}.png`} alt={weather?.weather[0].description} />
+            <p>Wind: {weather?.wind.speed} m/s</p> 
         </div>
     )
 }
